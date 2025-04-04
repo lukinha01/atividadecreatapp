86e16e4860aa51ac7a29643c7f5264ab9db107d0
lukinha01 <152975674+lukinha01@users.noreply.github.com>
2025-04-03 22:11:20 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -2,16 +2,73 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
+import { Avatar } from 'react-native-elements';
+import { Button } from 'react-native-elements';
+
+function login() {
   return (
     <View style={styles.container}>
-      <Text>lukinhas 2025</Text>
-      <Text>Rogerio Lucas do Nascimento Silva</Text>
-      <Text>17 anos</Text>
-      <Text> atleta</Text> 
-      <Text>futebol/volei</Text>
-      <Text>2026 é hexa!!</Text>
-      
+
+      <Avatar
+        size ='large'
+        rounded
+        icon={{name: 'user', type: 'font-awesome'}}
+        onPress={() => console.log("Works!")}
+        activeOpacity={0.7}
+        containerStyle={{marginLeft: 20, marginTop: 115}}
+      />
+      <Input
+        placeholder='Email'
+      />
+      <Input
+        placeholder='senha'
+      />
+      <Button
+        title="logar"
+      />
+      <Text> </Text>
+      <Button
+        title="cadastre-se"
+      />
+      <Text>esqueceu a senha</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function cadastroUsuario() {
+  return (
+    <View style={styles.container}>
+      <Text h1 style={styles.text}> CADASTRO</Text>
+      <Input
+        placeholder='Nome'
+      />
+      <Input
+        placeholder='Email'
+      />
+      <Input
+        placeholder='senha'
+      />
+      <Button
+        title="cadastrar-se"
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default function esqueceuSenha() {
+  return (
+    <View style={styles.container}>
+      <Text h1 style={styles.text}> ESQUECEU A SENHA</Text>
+      <Input
+        placeholder='Email'
+      />
+      <Button
+        title="enviar"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -20,8 +77,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:  '#808080',
+    backgroundColor: '#a0a0a0',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    marginTop: 115,
   },
 });
